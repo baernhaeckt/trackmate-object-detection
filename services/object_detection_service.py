6880d735b67c561244c547cd4d8baf9bafaaf7db
Oliver Hofmann <oliverhofmann@Olivers-MacBook-Pro-16.local>
2024-08-24 20:44:05 +0200

    Change yolo model to nano

--- services/object_detection_service.py
+++ services/object_detection_service.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 class ObjectDetectionService:
     def __init__(self):
-        self.object_detection_model = YOLO("./../model/yolov10m.pt")
+        self.object_detection_model = YOLO("./../model/yolov10n.pt")
 
     @staticmethod
     def _distance_calculator(box, img: np.ndarray) -> float:
