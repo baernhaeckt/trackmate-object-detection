4f47386846128a12d760a31659382ce6d200d8ca
Oliver Hofmann <oliverhofmann@Olivers-MacBook-Pro-16.local>
2024-08-25 08:27:11 +0200

    Create instance in every request

--- controllers/object_detection_controller.py
+++ controllers/object_detection_controller.py
@@ -8,7 +8,6 @@ from fastapi.responses import JSONResponse
 from services.object_detection_service import ObjectDetectionService
 
 router = APIRouter()
-object_detection_service = ObjectDetectionService()
 
 
 @router.post("/", tags=["predict detect"], status_code=200)
@@ -18,6 +17,7 @@ def predict_and_detect(file: UploadFile):
     with NamedTemporaryFile(delete=True, suffix=suffix) as tmp:
         shutil.copyfileobj(file.file, tmp)
 
+        object_detection_service = ObjectDetectionService()
         bounding_boxes = object_detection_service.predict_and_detect(tmp.name)
 
         return JSONResponse(content=bounding_boxes)
